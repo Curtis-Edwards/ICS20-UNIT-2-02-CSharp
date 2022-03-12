b58875bae66afe3eebd26ce7a8a9fa97ebc79018
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-12 02:36:06 +0000

    removed whitespace

--- main.cs
+++ main.cs
@@ -1,17 +1,16 @@
 // This program does basic math
 
-using System; 
+using System;
 
-public class Program 
+public class Program
 {
 
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
         Console.WriteLine("Area:");
         Console.WriteLine("12 * 7 = " + (12 * 7) + "cm²");
         Console.WriteLine("Perimeter:");
-        Console.WriteLine("2(12 + 7) = " + (2 * (12 + 7)) + "cm");
-        
+        Console.WriteLine("2(12 + 7) = " + (2 * (12 + 7)) + "cm");        
 
         Console.WriteLine("\nDone.");
     }
