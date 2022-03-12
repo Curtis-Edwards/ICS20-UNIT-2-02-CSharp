8a91ca325786e1434918947a02e860e65143a8b5
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-12 02:29:14 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+// This program does basic math
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("Area:");
+        Console.WriteLine("12 * 7 = " + (12 * 7) + "cm²");
+        Console.WriteLine("Perimeter:");
+        Console.WriteLine("2(12 + 7) = " + (2 * (12 + 7)) + "cm");
+        
+
+        Console.WriteLine("\nDone.");
+    }
+}
