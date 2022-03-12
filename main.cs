7c32885dd1ea33d9d80d5f26e837d0ce548ec3e4
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-03-12 02:42:56 +0000

    remvoed another whitespace

--- main.cs
+++ main.cs
@@ -10,7 +10,7 @@ public class Program
         Console.WriteLine("Area:");
         Console.WriteLine("12 * 7 = " + (12 * 7) + "cm²");
         Console.WriteLine("Perimeter:");
-        Console.WriteLine("2(12 + 7) = " + (2 * (12 + 7)) + "cm");       
+        Console.WriteLine("2(12 + 7) = " + (2 * (12 + 7)) + "cm");
 
         Console.WriteLine("\nDone.");
     }
